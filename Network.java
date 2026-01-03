0ae7ae1fac555d8f8681bb8e4de0cd31444419bf
Tal Gershtein <tal.gershtein@post.runi.ac.il>
2026-01-04 01:45:00 +0200

    Completed assignment

--- Network.java
+++ Network.java
@@ -115,10 +115,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
         for (int i = 0; i < userCount; i++) {
-            ans += users[i];
-            if (i < userCount - 1) ans += "\n";
+            ans += "\n" + users[i];
         }
         return ans;
     }
